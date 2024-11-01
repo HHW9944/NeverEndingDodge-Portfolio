5b7ac88a85b8206b76f99f2c488980293f4b5155
unknown <john9912@naver.com>
2024-11-01 23:01:09 +0900

    Shooter 컴포넌트가 이제 플레이어를 조준함

--- Assets/JongKwon(Temp)/Enemies/EnemyAI.cs
+++ Assets/JongKwon(Temp)/Enemies/EnemyAI.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Enemy �ϳ����� ������ ������Ʈ
 public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
@@ -20,16 +19,13 @@ public class EnemyAI : MonoBehaviour
     {
         if (player != null)
         {
-            // �÷��̾���� �Ÿ� ���
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
-            // �÷��̾ �ٶ� (Slerp ����ؼ� �ε巴�� ȸ��)
             if (isAlwaysRecognizePlayer || distanceToPlayer <= recognitionDistance)
             {
                 Vector3 direction = player.transform.position - transform.position;
                 Quaternion targetRotation = Quaternion.LookRotation(direction);
 
-                // !!! Player ������Ʈ�� �ݴ� �������� �ٶ󺻴ٸ� ���⸦ �������ּ��� !!!
                 targetRotation *= Quaternion.Euler(0, 180, 0);
 
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
--- Assets/JongKwon(Temp)/Enemies/Shooter.cs
+++ Assets/JongKwon(Temp)/Enemies/Shooter.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField]
-    GameObject missilePrefab;
-
-    [SerializeField]
-    float initialDelay = 2.0f; // 첫 발사까지의 대기 시간
-    [SerializeField]
-    float fireRate = 1.5f;     // 미사일 발사 주기
+    public GameObject player;
+    public GameObject missilePrefab;
+    public float initialDelay = 2.0f; // 첫 발사까지의 대기 시간
+    public float fireRate = 1.5f;     // 미사일 발사 주기
+    public float rotationSpeed = 5.0f;
 
     void Start()
     {
         // 지정된 시간 이후부터 일정한 간격으로 미사일 발사
         InvokeRepeating(nameof(FireMissile), initialDelay, fireRate);
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+            Vector3 direction = player.transform.position - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+            // targetRotation *= Quaternion.Euler(0, 180, 0);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
     void FireMissile()
