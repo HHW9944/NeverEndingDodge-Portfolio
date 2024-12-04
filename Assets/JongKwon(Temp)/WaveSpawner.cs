e11bf53071eeadfc77e255cff01259e2551c50ec
unknown <john9912@naver.com>
2024-12-04 14:38:24 +0900

    중간저장

--- Assets/JongKwon(Temp)/WaveSpawner.cs
+++ Assets/JongKwon(Temp)/WaveSpawner.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 public class WaveSpawner : MonoBehaviour
 {
     public float wave01StartTime = 5;
-    public float wave02StartTime = 10;
+    public float wave02StartTime = 3;
+    public float wave03StartTime = 3;
+    public float wave04StartTime = 3;
+    public float wave05StartTime = 3;
+
+    // 적, 미사일, 소행성 프리팹
+    public GameObject[] enemyPrefabs;
+    public GameObject[] missilePrefabs;
+    public GameObject[] obastaclePrefabs;
 
     void Start()
     {
@@ -14,12 +22,16 @@ public class WaveSpawner : MonoBehaviour
 
     IEnumerator SpawnWave()
     {
-        // x초 대기
         yield return new WaitForSeconds(wave01StartTime);
         Debug.Log($"First action after {wave01StartTime} seconds!");
 
-        // y초 대기
         yield return new WaitForSeconds(wave02StartTime);
         Debug.Log($"Second action after {wave02StartTime} seconds!");
+
+        yield return new WaitForSeconds(wave03StartTime);
+
+        yield return new WaitForSeconds(wave04StartTime);
+
+        yield return new WaitForSeconds(wave05StartTime);
     }
 }
